d4ee4afab63248718f98b45a382bfbd5fbd23580
WalkerKenneth <kennethesteban@hotmail.com>
2024-04-01 09:35:13 -0600

    Changes on data module to work with student class

--- week-10/data.py
+++ week-10/data.py
@@ -1,28 +1,51 @@
 import csv
+import student
 
 def import_data():
     student_list = []
     try:
-        with open('student_list.csv', 'r', encoding='utf-8') as file:
+        with open('week-10/student_list.csv', 'r', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 for key in row:
                     if key[len(key)-4:len(key)] == 'note':
                         row[key] = int(row[key])
-                student_list.append(row)
+                student_list.append(student(
+                    row['name'],
+                    row['group'],
+                    row['spanish_note'],
+                    row['english_note'],
+                    row['history_note'],
+                    row['science_note']
+                ))
     except FileNotFoundError:
         print('Document is not available')
     return student_list
 
 
-def export_data(list):
-    headers = []
-    try:
-        for key in list[0]:
-            headers.append(key)
-    except IndexError as ex:
-        print('Incorrect list provided')
-    with open('student_list.csv', 'w', encoding='utf-8') as file:
+def export_data(person_list):
+    list = []
+    headers = [
+        'name',
+        'group',
+        'spanish_note',
+        'english_note',
+        'history_note',
+        'science_note'
+    ]
+    for person in person_list:
+        list.append(
+            {
+                'name': person.name,
+                'group': person.group,
+                'spanish_note': person.spanish_note,
+                'english_note': person.english_note,
+                'history_note': person.history_note,
+                'science_note': person.science_note
+            }
+        )
+
+    with open('week-10/student_list.csv', 'w', encoding='utf-8') as file:
         writer = csv.DictWriter(file, headers)
         writer.writeheader()
         writer.writerows(list)
---
+++ week-10/student.py
@@ -0,0 +1,14 @@
+class Student:
+
+
+    def __init__(self) -> None:
+        pass
+
+
+    def __init__(self, name, group, spanish_note, english_note, history_note, science_note) -> None:
+        self.name = name
+        self.group = group
+        self.spanish_note = spanish_note
+        self.english_note = english_note
+        self.history_note = history_note
+        self.science_note = science_note
