27badfb954d58923797fa51ba0cfb04543bd25be
WalkerKenneth <kennethesteban@hotmail.com>
2024-05-07 10:27:24 -0600

    Commit for moved elements

--- week-10/student.py
+++
@@ -1,9 +0,0 @@
-class Student:
-
-    def __init__(self, name, group, spanish_note, english_note, history_note, science_note) -> None:
-        self.name = name
-        self.group = group
-        self.spanish_note = spanish_note
-        self.english_note = english_note
-        self.history_note = history_note
-        self.science_note = science_note
--- week-17/main.py
+++
@@ -1,7 +0,0 @@
-from principal_menu import PrincipalMenu
-
-def main():
-    PrincipalMenu()
-
-if __name__ == '__main__':
-    main()
