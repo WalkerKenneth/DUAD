b983d3cd8616e6fabe54b34d633ae1e7a8f8c4a1
WalkerKenneth <kennethesteban@hotmail.com>
2024-06-07 09:54:30 -0600

    Excercise number 5

--- module-2/week-5/week-5.js
+++ module-2/week-5/week-5.js
@@ -41,7 +41,7 @@ const wordList = (text) => {
         if (text[i] === ' ') {
             list.push(word);
             word = ''
-        } else if (parseInt(i)  === text.length - 1){
+        } else if (parseInt(i) === text.length - 1) {
             word += text[i];
             list.push(word);
         }
@@ -51,6 +51,50 @@ const wordList = (text) => {
     };
     return list;
 };
+
+//Exercise 5
+const studentResume = (student) => {
+    let gradesNumber = 0;
+    let gradeAvg = 0;
+    let highestGrade = '';
+    let tempHigh = 0;
+    let lowestGrade = '';
+    let tempLow = 0;
+
+
+    for (let key in Object.values(student.grades)) {
+        gradesNumber += 1;
+        gradeAvg += student.grades[key].grade;
+
+        if (highestGrade === '') {
+            highestGrade = student.grades[key].name;
+            tempHigh = student.grades[key].grade;
+        } else {
+            if (student.grades[key].grade > tempHigh) {
+                highestGrade = student.grades[key].name;
+                tempHigh = student.grades[key].grade;
+            }
+        }
+
+        if (lowestGrade === '') {
+            lowestGrade = student.grades[key].name;
+            tempLow = student.grades[key].grade;
+        } else {
+            if (student.grades[key].grade < tempLow) {
+                lowestGrade = student.grades[key].name;
+                tempLow = student.grades[key].grade;
+            }
+        }
+    }
+
+    let resume = {
+        name: student.name,
+        gradeAvg: gradeAvg / gradesNumber,
+        highestGrade: highestGrade,
+        lowestGrade: lowestGrade,
+    };
+    return resume;
+}
 
 //Exercise 1 test
 let names = ['Roberto', 'Carlos', 'Maria'];
@@ -68,7 +112,21 @@ let gradesCelsius = [0, 12, 34, 45, 68, 100];
 let gradesFarenheit = celsiusToFarenheit(gradesCelsius);
 console.log(gradesFarenheit);
 
-//Exercise 3 test
+//Exercise 4 test
 let text = 'This is a string!';
 let words = wordList(text);
 console.log(words);
+
+//Exercise 5 test
+let student = {
+    name: "John Doe",
+    grades: [
+        { name: "math", grade: 80 },
+        { name: "science", grade: 100 },
+        { name: "history", grade: 60 },
+        { name: "PE", grade: 90 },
+        { name: "music", grade: 98 }
+    ]
+};
+let resume = studentResume(student);
+console.log(resume);
